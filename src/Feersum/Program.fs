cea5590c359ad08109bf248f3fe5661a085cb980
Will Speak <lithiumflame@gmail.com>
2019-11-22 07:53:13 +0000

    Scruffy AST Node
    Beginning to sketch out the AST node type.

--- src/Feersum/Program.fs
+++ src/Feersum/Program.fs
@@ -2,7 +2,14 @@
 
 open System
 
+// The main AST Node type
+type AstNode =
+    | Atom of String
+    | Number of int
+    | Str of String
+    | Boolean of bool
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    Atom "hello world" |> printfn "%A"
     0 // return an integer exit code
