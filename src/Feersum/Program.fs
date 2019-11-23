bcb46a1e515d36dba754ecec963a21e970ca5d5c
Will Speak <lithiumflame@gmail.com>
2019-11-23 09:07:49 +0000

    Introduce REPL
    This just reads and prints strings for now. Sketching out the
    structure of a simplle interpretrer though.

--- src/Feersum/Program.fs
+++ src/Feersum/Program.fs
@@ -9,7 +9,30 @@ type AstNode =
     | Str of String
     | Boolean of bool
 
+/// Read a single line of user input and parse it into a
+/// syntax tree.
+let read () =
+    Console.Write "§> "
+    Console.Out.Flush()
+    Console.ReadLine()
+
+/// Take a syntax tree and evaluate it producing a value.
+let execute input =
+    input
+
+/// Print a value out to the console
+let print value =
+    value |> printfn "]= %A"
+
+
+/// Read, Execute, Print Loop
+///
+/// Repeatedly reads input and prints output
+let rec repl () =
+    (read() |> execute |> print)
+    repl()
+
 [<EntryPoint>]
 let main argv =
-    Atom "hello world" |> printfn "%A"
+    repl()
     0 // return an integer exit code
