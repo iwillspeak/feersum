9574eb2d15f1c6a1fcbdb9ee6030da16b3647361
Will Speak <lithiumflame@gmail.com>
2020-03-27 07:56:29 +0000

    Add Snapshot Tests for Parsing
    Adds some `Snapper` tests for the AST produced by parsing. This seems
    really neat and might come in useful to test the output of the specs
    too.

--- spec/conditionals.scm
+++ spec/conditionals.scm
@@ -1,3 +1,8 @@
 (if #t
     255
     -123)
+
+;; Hello isn't #f, therefore we should return "world"
+(if "hello"
+    "world"
+    -2)
--- test/Feersum.Tests/SpecTests.fs
+++ test/Feersum.Tests/SpecTests.fs
@@ -2,7 +2,10 @@ module SpecTests
 
 open Xunit
 open Compile
+open Syntax
 open System.IO
+open Snapper
+open Snapper.Attributes
 
 let specDir = Path.Join(__SOURCE_DIRECTORY__, "..", "..", "spec")
 
@@ -16,3 +19,9 @@ let ``spec tests compile`` s =
     match compileFile (Path.Join(specDir, s)) with
     | Ok _ -> ()
     | Error e -> failwithf "Compilation error: %A" e
+
+[<Theory>]
+[<MemberDataAttribute("listSpecs")>]
+let ``spect tests parse result`` s =
+    let tree = parseFile (Path.Join(specDir, s))
+    tree.ShouldMatchSnapshot(Core.SnapshotId(specDir, "Parse", s))
