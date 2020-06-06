72d92aefd22069f356e6467b21466f4f54a663c8
Will Speak <lithiumflame@gmail.com>
2020-06-06 08:42:29 +0100

    Execute Specs on Test
    Execute the snapshot if it compiles. Also moves all snapshots into the
    new `_snapshots` directory.

--- test/Feersum.Tests/SpecTests.fs
+++ test/Feersum.Tests/SpecTests.fs
@@ -6,8 +6,10 @@ open Syntax
 open System.IO
 open Snapper
 open Snapper.Attributes
+open System.Diagnostics
 
 let specDir = Path.Join(__SOURCE_DIRECTORY__, "..", "..", "spec")
+let snapDir = Path.Join(__SOURCE_DIRECTORY__, "_snapshots")
 
 let listSpecs =
     Directory.GetFiles(specDir, "*.scm", SearchOption.AllDirectories)
@@ -15,13 +17,18 @@ let listSpecs =
 
 [<Theory>]
 [<MemberDataAttribute("listSpecs")>]
-let ``spec tests compile`` s =
-    match compileFile (Path.Join(specDir, s)) with
-    | Ok _ -> ()
+let ``spec tests compile and run`` s =
+    let sourcePath = Path.Join(specDir, s)
+    match compileFile (sourcePath) with
+    | Ok _ ->
+        let exePath = Path.ChangeExtension(sourcePath, "exe")
+        let p = Process.Start("dotnet", exePath)
+        p.WaitForExit()
+        p.ExitCode.ShouldMatchChildSnapshot(s)
     | Error e -> failwithf "Compilation error: %A" e
 
 [<Theory>]
 [<MemberDataAttribute("listSpecs")>]
 let ``spect tests parse result`` s =
     let tree = parseFile (Path.Join(specDir, s))
-    tree.ShouldMatchSnapshot(Core.SnapshotId(specDir, "Parse", s))
+    tree.ShouldMatchSnapshot(Core.SnapshotId(snapDir, "Parse", s))
