e2352364e86b63b396d69f51529bcd9149845ee3
Will Speak <lithiumflame@gmail.com>
2020-09-02 18:25:34 +0100

    Add Catch-all Failure Message to Interpreter
    When load and store is not supported just return a generic error.

--- src/Feersum/Interpret.fs
+++ src/Feersum/Interpret.fs
@@ -68,9 +68,7 @@ let rec executeBound (env: IDictionary<string, SchemeValue>) (expr: BoundExpr) =
         let init = Option.map recurse maybeInit |> SchemeValue.fromOption
         match store with
         | StorageRef.Global id -> env.Add(id, init)
-        | StorageRef.Local idx -> failwith "Set local not implemented"
-        | StorageRef.Arg idx -> failwith "Set argument not implemented"
-        | StorageRef.Builtin idx -> failwith "builtins not implemented"
+        | _ -> failwithf "Storage not supported %A" store
         // actually using this value is difficult in Scheme so i'm  not sure it
         // matters _too_ much _what_ it is. It's definitely _not_ the init value
         // fo the new variable binding though.
@@ -85,12 +83,7 @@ let rec executeBound (env: IDictionary<string, SchemeValue>) (expr: BoundExpr) =
     | BoundExpr.Load l ->
         match l with
         | StorageRef.Global id ->  Map.tryFind id builtins |> Option.get
-        | StorageRef.Arg idx ->
-            failwith "Load argument not implemented"
-        | StorageRef.Local idx -> 
-            // TODO: implement local and global loads
-            failwith "local loads not implemented in the interpreter"
-        | StorageRef.Builtin idx -> failwith "builtins not implemented"
+        | _ -> failwithf "Storage not supported %A" l
     | BoundExpr.If(cond, ifTrue, maybeIfFalse) ->
         match (recurse cond) with
         | SchemeValue.Boolean false ->
