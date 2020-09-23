ce7f61af7984e2ced888fcafc6c9c621c8956b5d
Will Speak <lithiumflame@gmail.com>
2020-09-23 19:44:47 +0100

    Fixup Evaluation Tests
    Stub out position information for the evaluator.

--- test/Feersum.Tests/EvalTests.fs
+++ test/Feersum.Tests/EvalTests.fs
@@ -8,6 +8,11 @@ open Syntax
 let interpret = execute >> externalRepr
 let feeri = eval >> cilExternalRepr
 
+let private dummyLocation = Point(FParsec.Position("dummy", 0L, 0L, 0L))
+
+let n node =
+    { Kind = node; Location = dummyLocation }
+
 let evaluators = [|
         [| interpret |];
         [| feeri |]
@@ -21,41 +26,41 @@ let private tryReadSingle expr =
 [<Theory>]
 [<MemberData("evaluators")>]
 let ``Evaluate atoms`` evaluator =
-    Assert.Equal("#t", evaluator(Boolean true))
-    Assert.Equal("#f", evaluator(Boolean false))
-    Assert.Equal(@"""hello""", evaluator(Str "hello"))
-    Assert.Equal("1337", evaluator(Number 1337.0))
-    Assert.Equal("123.456", evaluator(Number 123.456))
+    Assert.Equal("#t", evaluator(Boolean true |> n))
+    Assert.Equal("#f", evaluator(Boolean false |> n))
+    Assert.Equal(@"""hello""", evaluator(Str "hello" |> n))
+    Assert.Equal("1337", evaluator(Number 1337.0 |> n))
+    Assert.Equal("123.456", evaluator(Number 123.456 |> n))
 
 [<Theory>]
 [<MemberData("evaluators")>]
 let ``Evaluate lists`` evaluator =
-    Assert.Equal("132", evaluator(Seq [ Boolean false; Number 132.0 ]))
-    Assert.Equal("#t", evaluator(Seq [ Boolean true ]))
+    Assert.Equal("132", evaluator(Seq [ Boolean false |> n; Number 132.0  |> n] |> n))
+    Assert.Equal("#t", evaluator(Seq [ Boolean true |> n ] |> n))
 
 // TODO: Empty program yields NULL for eval, but special undefined value for
 //       the interpreter. How can we represent undefined values in .NET?
 [<Fact>]
 let ``Evaluate empty program`` () =
-    Assert.Equal("; unspecified value", interpret(Seq [ ]))
-    Assert.Equal("()", feeri(Seq [ ]))
+    Assert.Equal("; unspecified value", interpret(Seq [ ] |> n))
+    Assert.Equal("()", feeri(Seq [ ] |> n))
 
 [<Fact>]
 let ``Evaluate lambdas returns`` () =
     Assert.Equal("123", feeri(Form [
-        Form [ Ident "lambda"; Form [ Ident "x" ]; Ident "x"];
-        Number 123.0]))
+        Form [ Ident "lambda" |> n ; Form [ Ident "x" |> n ] |> n; Ident "x" |> n] |> n;
+        Number 123.0 |> n] |> n))
 
 // TODO: Number builtins are only implemented in the interpreter right now.
 [<Fact>]
 let ``Evaluate builtins`` () =
     let evaluator = interpret
-    Assert.Equal("19", evaluator(Form [ Ident "+"; Number 10.0; Number 9.0 ]))
-    Assert.Equal("901", evaluator(Form [ Ident "+"; Number 901.0 ]))
-    Assert.Equal("90", evaluator(Form [ Ident "*"; Number 10.0; Number 9.0 ]))
-    Assert.Equal("901", evaluator(Form [ Ident "+"; Form [ Ident "*"; Number 100.0; Number 9.0 ]; Number 1.0]))
-    Assert.Equal("1", evaluator(Form [ Ident "-"; Number 10.0; Number 9.0 ]))
-    Assert.Equal("2", evaluator(Form [ Ident "/"; Number 16.0; Number 8.0 ]))
+    Assert.Equal("19", evaluator(Form [ Ident "+" |> n; Number 10.0 |> n; Number 9.0 |> n ] |> n))
+    Assert.Equal("901", evaluator(Form [ Ident "+" |> n; Number 901.0 |> n ] |> n))
+    Assert.Equal("90", evaluator(Form [ Ident "*" |> n; Number 10.0 |> n; Number 9.0 |> n ] |> n))
+    Assert.Equal("901", evaluator(Form [ Ident "+" |> n; Form [ Ident "*" |> n; Number 100.0 |> n; Number 9.0 |> n ]|> n; Number 1.0 |> n] |> n))
+    Assert.Equal("1", evaluator(Form [ Ident "-" |> n; Number 10.0 |> n; Number 9.0 |> n ] |> n))
+    Assert.Equal("2", evaluator(Form [ Ident "/" |> n; Number 16.0 |> n; Number 8.0 |> n ] |> n))
 
 [<Theory>]
 [<InlineData("(+ 3 4)", "7")>]
@@ -90,8 +95,8 @@ let ``evaluate artithemtic ops`` expr result =
 [<InlineData(">=")>]
 [<InlineData("<=")>]
 let ``comp ops return true for simple cases`` op =
-    Assert.Equal("#t", feeri(Form [ Ident op ]))
-    Assert.Equal("#t", feeri(Form [ Ident op; Number 123.456 ]))
+    Assert.Equal("#t", feeri(Form [ Ident op  |> n] |> n))
+    Assert.Equal("#t", feeri(Form [ Ident op |> n; Number 123.456 |> n ] |> n))
 
 [<Theory>]
 [<InlineData("(= 23 4234 234)", "#f")>]
