c1552dbe1cd710acac4d6bfa427dc06eae40c2e9
Will Speak <lithiumflame@gmail.com>
2020-09-30 20:09:48 +0100

    Add Test for Issue 4
    Fixes: #4

---
+++ spec/issue-4.scm
@@ -0,0 +1,13 @@
+(define (make-counter n)
+    (lambda ()
+        (set! n (+ 1 n))
+        n))
+(define from-ten (make-counter 10))
+(define from-ton (make-counter 100))
+
+(display (from-ten))
+(display (from-ton))
+
+;; This should return 102 + 12
+(display (+ (from-ton) (from-ten)))
+(+ (from-ton) (from-ten))
