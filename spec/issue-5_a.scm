eba666fbf5949c16bca64fffecb6d0b56c9b7616
Will Speak <lithiumflame@gmail.com>
2020-10-16 08:58:51 +0100

    Add Specs for Issue 5
    Fixes #5

---
+++ spec/issue-5_a.scm
@@ -0,0 +1,7 @@
+((lambda (a)
+    (display
+        (if #f
+              (lambda () a)
+            a))
+    a)
+  101) ; -> should be 101, but is '()
---
+++ spec/issue-5_b.scm
@@ -0,0 +1,14 @@
+(define (check n)
+    (letrec ((odd? (lambda (m)
+                (if (= m 0)
+                    #f
+                    (even? (- m 1)))))
+            (even? (lambda (m)
+                (if (= m 0)
+                    #t
+                    (odd? (- m 1))))))
+        (even? n)))
+
+(display (check 54))(newline)
+(display (check 7))(newline)
+(check 123)
