0fb1f3929fd86b580bf12db92506dc5101b322ef
Will Speak <lithiumflame@gmail.com>
2020-10-20 14:01:18 +0100

    Dump Debug Info

--- src/dbgdumper/Program.fs
+++ src/dbgdumper/Program.fs
@@ -4,17 +4,49 @@ open System
 open Mono.Cecil
 open Mono.Cecil.Rocks
 open Mono.Cecil.Cil
+open Newtonsoft.Json
 
-let private dumpInfo (path: string) =
+
+let private seqOfMonoCol (col: Mono.Collections.Generic.Collection<'t>) =
+    Seq.unfold (fun i ->
+        if i < col.Count then
+            Some(col.[i], i + 1)
+        else
+            None)
+        0
+
+let private dumpMethod (meth: MethodDefinition) =
+    printfn ">> Method: %A" meth
+    let sequencePoints =
+        seqOfMonoCol meth.DebugInformation.SequencePoints
+        |> Seq.map (fun x -> (x.Offset, x))
+        |> Map.ofSeq
+    seqOfMonoCol meth.Body.Instructions
+    |> Seq.indexed
+    |> Seq.iter (fun (i, il) ->
+        match Map.tryFind i sequencePoints with
+        | Some(point) ->
+            printfn "  %A (%d:%d %d:%d)" point.Document.Url point.StartLine point.StartColumn point.EndLine point.EndColumn
+        | None -> ()
+        printfn "  %A" il)
+    ()
+
+let private dumpType (ty: TypeDefinition) =
+    printfn "> Ty: %A" ty
+    seqOfMonoCol ty.Methods
+    |> Seq.iter dumpMethod
+    ()
+
+let private dumpAssm (path: string)  =
     let opts = ReaderParameters(ReadingMode.Immediate)
     opts.ReadSymbols <- true
     let assm = AssemblyDefinition.ReadAssembly(path, opts)
-    printfn "Body: %A" assm.MainModule.EntryPoint.Body.Instructions
-    printfn "Debug Info Scopes: %A" assm.MainModule.EntryPoint.DebugInformation.Scope
-    printfn "Debug Info Seq Points: %A" assm.MainModule.EntryPoint.DebugInformation.SequencePoints
-    ()
+    printfn "Runtime: %A" assm.MainModule.Runtime
+    printfn "Runtime Version: %A" assm.MainModule.RuntimeVersion
+    seqOfMonoCol assm.MainModule.Types
+    |> Seq.iter dumpType
 
 [<EntryPoint>]
 let main argv =
-    Seq.iter dumpInfo argv
+    Seq.iter dumpAssm argv
     0 // return an integer exit code
