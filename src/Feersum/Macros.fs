dc13e123b34f33bedb0c97ec4a392fed76b48564
Will Speak <lithiumflame@gmail.com>
2020-10-31 16:20:56 +0000

    Macro Variable Patterns
    Macro variables match anything and bind it to the syntax.

--- src/Feersum/Macros.fs
+++ src/Feersum/Macros.fs
@@ -5,16 +5,21 @@ open Syntax
 /// macro should match.
 type MacroPattern =
     | Constant of SyntaxConstant
+    | Variable of string
+
+type MacroBinding = (string * AstNode)
 
 /// Attempt to match a pattern against a syntax tree. Returns `Ok` if the
 /// pattern matches. Returns `Err` if the pattern does not match the given node.
-let macroMatch (pat: MacroPattern) (ast: AstNode): Result<unit,unit> =
+let macroMatch (pat: MacroPattern) (ast: AstNode): Result<MacroBinding list,unit> =
     match pat with
     | Constant c ->
         match ast.Kind with
         | AstNodeKind.Constant k ->
             if k = c then
-                Result.Ok ()
+                Result.Ok []
             else
                 Result.Error ()
         | _ -> Result.Error ()
+    | Variable v ->
+        Result.Ok [(v, ast)]
