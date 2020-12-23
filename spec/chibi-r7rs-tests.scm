b0724580cfb45bbf135aa8c1c46089adc0238397
Will Speak <lithiumflame@gmail.com>
2020-12-23 12:46:14 +0000

    Enable Chibi Symbol Tests

--- spec/chibi-r7rs-tests.scm
+++ spec/chibi-r7rs-tests.scm
@@ -1228,28 +1228,28 @@
 
 (test-begin "6.5 Symbols")
 
-;; (test #t (symbol? 'foo))
-;; (test #t (symbol? (car '(a b))))
-;; (test #f (symbol? "bar"))
-;; (test #t (symbol? 'nil))
-;; (test #f (symbol? '()))
-;; (test #f (symbol? #f))
+(test #t (symbol? 'foo))
+(test #t (symbol? (car '(a b))))
+(test #f (symbol? "bar"))
+(test #t (symbol? 'nil))
+(test #f (symbol? '()))
+(test #f (symbol? #f))
 
-;; (test #t (symbol=? 'a 'a))
-;; (test #f (symbol=? 'a 'A))
-;; (test #t (symbol=? 'a 'a 'a))
-;; (test #f (symbol=? 'a 'a 'A))
+(test #t (symbol=? 'a 'a))
+(test #f (symbol=? 'a 'A))
+(test #t (symbol=? 'a 'a 'a))
+(test #f (symbol=? 'a 'a 'A))
 
-;; (test "flying-fish"     
-;; (symbol->string 'flying-fish))
-;; (test "Martin" (symbol->string 'Martin))
-;; (test "Malvina" (symbol->string (string->symbol "Malvina")))
+(test "flying-fish"     
+(symbol->string 'flying-fish))
+(test "Martin" (symbol->string 'Martin))
+(test "Malvina" (symbol->string (string->symbol "Malvina")))
 
-;; (test 'mISSISSIppi (string->symbol "mISSISSIppi"))
-;; (test #t (eq? 'bitBlt (string->symbol "bitBlt")))
-;; (test #t (eq? 'LollyPop (string->symbol (symbol->string 'LollyPop))))
-;; (test #t (string=? "K. Harper, M.D."
-;;                    (symbol->string (string->symbol "K. Harper, M.D."))))
+(test 'mISSISSIppi (string->symbol "mISSISSIppi"))
+(test #t (eq? 'bitBlt (string->symbol "bitBlt")))
+(test #t (eq? 'LollyPop (string->symbol (symbol->string 'LollyPop))))
+(test #t (string=? "K. Harper, M.D."
+                   (symbol->string (string->symbol "K. Harper, M.D."))))
 
 (test-end)
 
