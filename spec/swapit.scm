42a81661e58dd6629775577cf19c3e25f6d2167a
Will Speak <lithiumflame@gmail.com>
2020-12-24 08:46:49 +0000

    Add Macro Swap Example
    This swaps the values _inside_ two variables rther than swapping the
    order of two values.

--- spec/swapit.scm
+++ spec/swapit.scm
@@ -3,6 +3,16 @@
         (you me))
         you))
 
+(let ((x 1) (y 2))
+    (define-syntax swap!
+        (syntax-rules ()
+            ((swap! a b)
+             (let ((tmp a))
+                (set! a b)
+                (set! b tmp)))))
+    (swap! x y)
+    (display (list x y)))
+
 (display (swap "Fooble" "Barble"))
 
 (= (swap 123 "Test") 123)
