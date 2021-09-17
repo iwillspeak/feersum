45487015164246604c4b53179b010aee3bfca0b8
Will Speak <lithiumflame@gmail.com>
2021-09-17 18:31:44 +0100

    Refactor Run Stub
    This is now a _little_ more ergonomic. It takes a spec name and
    compiles and executes it. More could still be done on normalising the
    name but this should be useful for now.

---
+++ runspec.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+    cat <<EOF
+Usage: runspec.sh <specname>
+
+Arguments:
+  <specname>       The name of the specfile to compile and run.
+EOF
+    exit -1
+fi
+
+specname=`fd $1 spec/`
+if [ $? -ne 0 ]
+then
+    echo >2 "Can't find spec file $1"
+fi
+
+specbin=spec/bin/$(basename ${specname/.scm/.exe})
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+set -euo pipefail
+
+dotnet build --configuration Release src/Feersum/Feersum.fsproj > /dev/null && \
+    dotnet src/Feersum/bin/Release/net5.0/Feersum.dll ${specname} --outputtype exe -o ${specbin} && \
+    dotnet ${specbin}
