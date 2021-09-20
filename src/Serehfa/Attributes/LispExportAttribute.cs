8d3f016a0d48062ee830434ea58efb9f891da063
Will Speak <lithiumflame@gmail.com>
2021-09-20 18:06:41 +0100

    Document Attributes

--- src/Serehfa/Attributes/LispExportAttribute.cs
+++ src/Serehfa/Attributes/LispExportAttribute.cs
@@ -2,14 +2,20 @@ using System;
 
 namespace Serehfa.Attributes
 {
+    /// <summary>Attribute to mark an exported lisp element</summary>
     [AttributeUsage(AttributeTargets.Method)]
     public class LispExportAttribute : Attribute
     {
+        /// <summary>
+        /// Initialise an instance of <see cref="LispExportAttribute" />
+        /// with the given external <paramref param="name" />.
+        /// </summary>
         public LispExportAttribute(string name)
         {
             Name = name;
         }
 
+        /// <summary>Gets the exported name of this builtin.</summary>
         public string Name { get; }
     }
 }
--- src/Serehfa/Attributes/LispLibraryAttribute.cs
+++ src/Serehfa/Attributes/LispLibraryAttribute.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 
 namespace Serehfa.Attributes
 {
-    /// <summary>Attribute to mark scheme libraries</summary>
+    /// <summary>Attribute to mark scheme libraries.</summary>
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class LispLibraryAttribute : Attribute
     {
-        /// <summary>Initialise an instance of <see cref="LispLibraryAttribute" /></summary>
+        /// <summary>
+        /// Initialise an instance of <see cref="LispLibraryAttribute" />
+        /// </summary>
         /// <param name="name">The library name.</param>
         public LispLibraryAttribute(params string[] name)
         {
             Name = name;
         }
 
-        /// <summary>The libraries name</summary>
-
+        /// <summary>The library's name</summary>
         public IReadOnlyCollection<string> Name { get; }
     }
 }
