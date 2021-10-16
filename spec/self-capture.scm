35b3aa594cb308eed9efa86628b022a1939dcc96
Will Speak <lithiumflame@gmail.com>
2021-10-16 15:59:58 +0100

    Improve Comments in Self Capture
    Add more words to help future debugging of the self-capture case.

--- spec/self-capture.scm
+++ spec/self-capture.scm
@@ -1,11 +1,21 @@
 (import (scheme base)
     (scheme write))
 
-(define (foo)   ;  [], [Local 0]            -> [], [Local 0]
-  (define (bar x)  ; [Local 0], [Arg 0]     -> [Environment 0], [Arg 0]
+;; Captured Local Compaction (shadow locals) Test
+;
+; In this example `foo` has two locals. The first is the function `bar` and the
+; second is the initial value `baz`. Once we have performed free variable
+; analysis on the function we identify that `Local 0` (bar) is captured. We
+; want the Lower pass to re-write `baz` from `Local 1` down to `Local 0` in this
+; case, as well as re-writing the references to the captured value to the env.
+;
+;                    captures, locals -- Lower --> captures, locals                  
+(define (foo)      ; [], [Local 0; Local 1]     -> [], [Local 0]
+  (define (bar x)  ; [Local 0], [Arg 0]         -> [Environment 0], [Arg 0]
     (if (< x 10)
       x
       (bar (- x 1))))
-  (bar 10))
+  (define baz 10)
+  (bar baz))
 
 (display (foo))(newline)
