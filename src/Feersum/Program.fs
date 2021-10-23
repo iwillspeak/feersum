948adde2a2ef1b3c11a1bcfe234327678306a6cc
Will Speak <lithiumflame@gmail.com>
2021-10-23 14:06:34 +0100

    Add Flag to Skip Deps Files
    We don't need these when compiling properly

--- src/Feersum/Program.fs
+++ src/Feersum/Program.fs
@@ -19,6 +19,7 @@ type CliArguments =
     | Configuration of BuildConfiguration
     | OutputType of OutputType
     | CoreLibPath of string
+    | GenerateDeps of bool
     | [<AltCommandLine("-r")>] Reference of string
     | [<AltCommandLine("-o")>] Output of string
     | [<MainCommand>] Sources of source_file: string list
@@ -30,6 +31,7 @@ type CliArguments =
             | Configuration _ -> "The build configuration (Debug / Release)."
             | OutputType _ -> "The output type (Lib / Exe / Script)."
             | CoreLibPath _ -> "Location of mscorelib.dll, or System.Runtime.dll."
+            | GenerateDeps _ -> "Generate .deps.json and .runtimeconfig.json stubs."
             | Reference _ -> "Compiled Scheme assembly to reference."
             | Output _ -> "The output path to write compilation results to."
             | Sources _ -> "Scheme source files for compilation."
@@ -127,6 +129,7 @@ let main argv =
     let options =
         { CompilationOptions.Create buildConfig outputType with
             References = args.GetResults Reference
+            GenerateDepsFiles = (args.TryGetResult GenerateDeps) |> Option.defaultValue true 
             MsCorePaths = args.GetResults CoreLibPath }
 
     match args.GetResult(Sources, defaultValue = []), args.TryGetResult(Output) with
