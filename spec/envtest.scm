5f104a44f90f918c312bebfd542e023016eb58a3
Will Speak <lithiumflame@gmail.com>
2021-11-20 11:33:40 +0000

    Thread Linked Environments
    Don't create otherwise empty environments when only the parent is
    catpured. Instead these methods can become instance methods on the
    parent environment instead. We thread the this pointer and avoid some
    overhead and indirection this way.

--- spec/envtest.scm
+++ spec/envtest.scm
@@ -6,4 +6,13 @@
     (+ a b))
   bar)
 
-(display ((foo 123) 4))
+(display ((foo 123) 4))(newline)
+
+(define (test a b)
+  (define (do-stuff)
+    (define (things c)
+      (+ a b c))
+    (things 12))
+  (do-stuff))
+
+(display (test 1 2))(newline)
