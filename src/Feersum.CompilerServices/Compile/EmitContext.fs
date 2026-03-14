e925b5d789c6e1fabcf9934737405aea82b01b2d
Will Speak <lithiumflame@gmail.com>
2026-03-14 08:01:38 +0000

    refactor: Deconstruct Emitter and Compilation
    Split the Emitter and Compilation logic into separate files. The
    emitter has become a bit of a sprawling mess. This attempts to separate
    out the context logicic from the main emittting logic while also
    smimming the small `Compilination` API off the top. This then leaves
    us with:
    
     * `EmitContext.fs` - the context and helper functions for the emitter
     * `Emit.fs` - the main emitter logic
     * `Compiler.fs` - marshalling compilation inputs into open streams

---
+++ src/Feersum.CompilerServices/Compile/EmitContext.fs
@@ -0,0 +1,57 @@
+namespace Feersum.CompilerServices.Compile
+
+open Mono.Cecil
+open Mono.Cecil.Cil
+open System.Collections.Generic
+
+/// Capture Environment Kind
+///
+/// Capture environments are either standard environments containing fields used
+/// to store captured values; or link environments.
+type EnvInfo =
+    /// A standard environment represents a closure type containing each
+    /// captured value as a field. If the environment has a parent then that is
+    /// stored as the final field.
+    | Standard of local: VariableDefinition * ty: TypeDefinition * parent: EnvInfo option
+    /// Link environments represent the case when only the parent environment
+    /// is captured. In this case we can re-use the parent type and pointer at
+    /// runtime. This saves the cost of a new type just for this closure's
+    /// methods, and saves indirection on lookups for deeply nested captures.
+    | Link of EnvInfo
+
+module private EnvUtils =
+    /// Get the local variable used to store the current method's environment
+    let getLocal =
+        function
+        | Standard(local, _, _) -> local |> Some
+        | _ -> None
+
+    /// Get the type part of the environment info.
+    let rec getType =
+        function
+        | Standard(_, ty, _) -> ty
+        | Link inner -> getType inner
+
+    /// Get the parent of the given environment.
+    let rec getParent =
+        function
+        | Standard(_, _, parent) -> parent
+        | Link inner -> Some(inner)
+
+/// Type to Hold Context While Emitting IL
+type EmitCtx =
+    { Assm: AssemblyDefinition
+      IL: ILProcessor
+      Locals: VariableDefinition list
+      Parameters: ParameterDefinition list
+      DebugDocuments: Dictionary<string, Document>
+      mutable NextLambda: int
+      ScopePrefix: string
+      EmitSymbols: bool
+      Exports: Map<string, string>
+      Externs: Map<string, TypeDefinition>
+      Environment: EnvInfo option
+      ProgramTy: TypeDefinition
+      mutable Initialisers: Map<string, MethodReference>
+      mutable Libraries: Map<string, TypeDefinition>
+      Core: CoreTypes }
