16e8730f8fc6b72d34e8f932bfdbbcfae1e66c1f
Will Speak <lithiumflame@gmail.com>
2026-04-03 09:27:12 +0100

    chore: Annotate Macro Expansion Tests with Expected Output
    Clarify that this set of tests _shouldn't_ fail. And add in the expected
    output as comments for future reference.

--- spec/fail/my-macros.scm
+++ spec/fail/my-macros.scm
@@ -24,24 +24,24 @@
 
 (define-syntax my-vec-macro
   (syntax-rules ()
-    ((_ #(a b c)) (list a b c))
+    ((_ #(a b c)) (list '(a b) c))
     ((_ #(a ...)) (list a ...))))
 
-(pp (my-vec-macro #(1 2 3)))  ; => (1 2 3)
+(pp (my-vec-macro #(1 2 3)))  ; => ((1 2) 3)
 (pp (my-vec-macro #(1 2 3 4 5)))  ; => (1 2 3 4 5)
-(pp (my-vec-macro #(1 2)))  ; => (1 2 3 4 5)
+(pp (my-vec-macro #(1 2)))  ; => (1 2)
 
 (define-syntax my-greedy-macro
   (syntax-rules ()
     ((_ a b ... c) (list 'a-got: a 'b-got: '(b ...) 'c-got: 'c))))
 
-(pp (my-greedy-macro 1 2 3))
-(pp (my-greedy-macro 1 2 3 4 5))
-(pp (my-greedy-macro 1 (2 3) (4 5)))
-(pp (my-greedy-macro 1 2))
+(pp (my-greedy-macro 1 2 3))         ;; => (a-got: 1 b-got: (2) c-got: 3)
+(pp (my-greedy-macro 1 2 3 4 5))     ;; => (a-got: 1 b-got: (2 3 4) c-got: 5)
+(pp (my-greedy-macro 1 (2 3) (4 5))) ;; => (a-got: 1 b-got: ((2 3)) c-got: (4 5))
+(pp (my-greedy-macro 1 2))           ;; => (a-got: 1 b-got: () c-got: 2)
 
 (define-syntax my-repeating-macro
   (syntax-rules ()
     ((_ #(a ...) #(b ...)) (list '(a . b) ...))))
 
-(pp (my-repeating-macro #(1 2 3) #(4 5 6)))
+(pp (my-repeating-macro #(1 2 3) #(4 5 6))) ;; => ((1 . 4) (2 . 5) (3 . 6))
